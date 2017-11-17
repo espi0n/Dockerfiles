452adad8d119d8aecda8c36601ed7452b61423da
Chan9390 <bnchandrapal@gmail.com>
2017-11-17 12:33:39 +0530

    Added logic to entrypoint.sh

--- tinfoleak/entrypoint.sh
+++ tinfoleak/entrypoint.sh
@@ -1,4 +1,12 @@
 echo "[+] Executing command: tinfoleak.py $@"
 python tinfoleak.py $@
+if [ $# -eq 0 ] ; then
+ echo "[-] No arguments supplied. Exiting"
+ exit
+fi
+if [[ $@ == *'-h'* ]] ; then
+ echo "[-] Exiting the help"
+ exit
+fi
 echo "[+] Starting HTTP Server"
 python -m SimpleHTTPServer 8000
